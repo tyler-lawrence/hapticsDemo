1fac615298a73737ea303f275c78499ec166ec01
TylerLawrence <tyler.lawrence15@gmail.com>
2022-02-08 15:52:43 -0500

    Initial Commit

---
+++ hapticsDemo/hapticsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  hapticsDemoApp.swift
+//  hapticsDemo
+//
+//  Created by Tyler Lawrence on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct hapticsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
