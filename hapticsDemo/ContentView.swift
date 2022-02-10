487db929abea7a7801276e3859a222ca1eff879f
TylerLawrence <tyler.lawrence15@gmail.com>
2022-02-10 09:47:40 -0500

    added full product

--- hapticsDemo/ContentView.swift
+++ hapticsDemo/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 import CoreHaptics
 
-// custom button view for haptics
 struct HapticsButtonView: View {
     
     @State var hapticLabel: String = ""
@@ -26,9 +25,80 @@ struct HapticsButtonView: View {
 
 struct ContentView: View {
     
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            HapticsButtonView(hapticLabel: "error", color: .red)
+                .onTapGesture(perform: errorHaptic)
+            HapticsButtonView(hapticLabel: "warning", color: .yellow)
+                .onTapGesture(perform: warningHaptic)
+            HapticsButtonView(hapticLabel: "success", color: .green)
+                .onTapGesture(perform: successHaptic)
+            HapticsButtonView(hapticLabel: "custom", color: .blue)
+                .onAppear(perform: prepareHaptics)
+                .onTapGesture(perform: complexSuccess)
+        }
     }
+    
+    // Simple UI Kit haptics
+    func successHaptic(){
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func warningHaptic(){
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.warning)
+    }
+    
+    func errorHaptic(){
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.error)
+    }
+    
+    // CoreHaptics
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
     
 }
 
